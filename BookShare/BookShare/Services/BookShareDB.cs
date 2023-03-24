50a155809c72720b63da313c806a50ca8800ed1c
Taha Hammouz <t.r.hamooz@gmail.com>
2023-03-24 14:49:40 +0200

    Set-Up-Server-Side

--- BookShare/BookShare/Services/BookShareDB.cs
+++ BookShare/BookShare/Services/BookShareDB.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Firebase.Database;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using BookShare.Models;
+using Firebase.Database.Query;
+using System.Threading.Tasks;
 
 namespace BookShare.Services
 {
     internal class BookShareDB
     {
+        FirebaseClient client;
+
+        public BookShareDB()
+        {
+            client = new FirebaseClient("https://bookshare-33c3f-default-rtdb.europe-west1.firebasedatabase.app/");
+        }
+        public async Task AddBook(Book book)
+        {
+            await client.Child("books").PostAsync(book);
+        }
+
+
     }
+
 }
