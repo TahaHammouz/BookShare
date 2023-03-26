2f5284cc4455da6cf8a0aa5d98cc22fc5ac837c7
Taha Hammouz <t.r.hamooz@gmail.com>
2023-03-26 17:29:10 +0300

    dummy data

---
+++ BookShare/BookShare/ViewModels/BookCardViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+
+namespace BookShare.ViewModels
+{
+    public class BookCardViewModel : INotifyPropertyChanged
+    {
+        private string _imagePath;
+        public string ImagePath
+        {
+            get => _imagePath;
+            set
+            {
+                _imagePath = value;
+                OnPropertyChanged(nameof(ImagePath));
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string _bookName;
+        public string BookName
+        {
+            get => _bookName;
+            set
+            {
+                _bookName = value;
+                OnPropertyChanged(nameof(BookName));
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- BookShare/BookShare/ViewModels/SearchViewModel.cs
+++ BookShare/BookShare/ViewModels/SearchViewModel.cs
@@ -1,10 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace BookShare.ViewModels
 {
     internal class SearchViewModel
     {
+        public List<BookCardViewModel> BookCards { get; set; }
+
+        public SearchViewModel()
+        {
+            // Dummy data for testing
+            BookCards = new List<BookCardViewModel>
+            {
+                new BookCardViewModel
+                {
+                    ImagePath = "image 1.jpg",
+                    Name = "John Smith",
+                    BookName = "The Great Gatsby",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+                },
+                new BookCardViewModel
+                {
+                    ImagePath = "image2.jpg",
+                    Name = "Jane Doe",
+                    BookName = "To Kill a Mockingbird",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+                },
+                new BookCardViewModel
+                {
+                    ImagePath = "image3.jpg",
+                    Name = "Bob Johnson",
+                    BookName = "1984",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+                },
+                                new BookCardViewModel
+                {
+                    ImagePath = "image 1.jpg",
+                    Name = "John Smith",
+                    BookName = "The Great Gatsby",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+                },                new BookCardViewModel
+                {
+                    ImagePath = "image 1.jpg",
+                    Name = "John Smith",
+                    BookName = "The Great Gatsby",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+                },                new BookCardViewModel
+                {
+                    ImagePath = "image 1.jpg",
+                    Name = "John Smith",
+                    BookName = "The Great Gatsby",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+                },
+            };
+        }
     }
 }
