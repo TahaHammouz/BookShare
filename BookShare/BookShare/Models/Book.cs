824c9f8359355626f1e13f62c131319f24841155
aboodjallad <abood.jallad.2001@gmail.com>
2023-03-27 17:50:05 +0300

    Get  all Posts from database

--- BookShare/BookShare/Models/Book.cs
+++ BookShare/BookShare/Models/Book.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 namespace BookShare.Models
-{
-	public class Book
-	{
-		public Book()
-		{
-		}
-	}
+{ 
+    public class Book 
+    { 
+        public Book() 
+        { 
+        } 
+        public string Username { get; set; } 
+        public string Bookname { get; set; } 
+        public string Details { get; set; } 
+        public string Contactlink { get; set; } 
+        public string Id { get; set; } 
+        public string Status { get; set; } 
+    } 
 }
-
