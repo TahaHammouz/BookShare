bc84909e5d6e900d9bedb2f6ed8d4a13afad9477
Halbonislam <islamhalboni.fw@gmail.com>
2023-04-06 01:51:12 +0300

    add new properties

--- BookShare/BookShare/Models/User.cs
+++ BookShare/BookShare/Models/User.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 namespace BookShare.Models
 {
-	public class User
-	{
-		public User()
-		{
-		}
-	}
+    public class User
+    {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Faculty { get; set; }
+        public string Gender { get; set; }
+
+    }
 }
+
 
