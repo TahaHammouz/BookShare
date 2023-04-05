565cf8b86096919944b71c4ef243126ada95a71a
Halbonislam <islamhalboni.fw@gmail.com>
2023-04-06 01:50:54 +0300

    Add new model class

---
+++ BookShare/BookShare/Models/UserDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShare.Models
+{
+    public class UserDatabase
+    {
+        public string Uid { get; set; }
+        public string Email { get; set; }
+        public string Faculty { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+    }
+}
