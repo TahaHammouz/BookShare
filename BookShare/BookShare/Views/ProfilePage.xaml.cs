c21c17505bda6f0b1e8f1178147bfde06f840d81
abedalaziz-kharobi <abedmk37@gmail.com>
2023-04-09 05:12:57 +0300

    BS-65 Design profile page and add "Edit" and "Delete" buttons to the donated books section.

--- BookShare/BookShare/Views/ProfilePage.xaml.cs
+++ BookShare/BookShare/Views/ProfilePage.xaml.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Rg.Plugins.Popup;
+using Xamarin.CommunityToolkit.PlatformConfiguration.iOSSpecific;
 
 namespace BookShare.Views
 {
@@ -15,6 +20,32 @@ namespace BookShare.Views
         public ProfilePage()
         {
             InitializeComponent();
+
+
         }
+        private void editProfile(object sender, EventArgs e)
+        {
+            // Handle the tap event here
+        }
+
+        
+        private async void ellipesesTapped(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Menu", "Cancel", null, "edit", "delete");
+
+            switch (action)
+            {
+                case "edit":
+                    // Handle option 1
+                    break;
+                case "delete":
+                    // Handle option 2
+                    break;
+                default:
+                    // Handle cancel or unknown option
+                    break;
+            }
+        }
+
     }
 }
