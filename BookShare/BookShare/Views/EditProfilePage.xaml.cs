24f16940828164c2580a2d24e7b31a2b6ad309a5
Taha Hammouz <t.r.hamooz@gmail.com>
2023-04-18 15:39:26 +0200

    BS-128 Design edit profile page

--- BookShare/BookShare/Views/EditProfilePage.xaml.cs
+++ BookShare/BookShare/Views/EditProfilePage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using BookShare.ViewModels;
+using BookShare.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,20 @@ namespace BookShare.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EditProfilePage : ContentPage
     {
-        public EditProfilePage()
+        public EditProfilePage(Models.User user)
         {
             InitializeComponent();
+            BindingContext = new EditProfileViewModel(user);
+            MessagingCenter.Subscribe<EditProfileViewModel>(this, "NavigateToNewPage", async (sender) =>
+            {
+                await Navigation.PushAsync(new ProfilePage());
+            });
+
         }
+        public EditProfilePage()
+        {
+
+        }
+
     }
 }
